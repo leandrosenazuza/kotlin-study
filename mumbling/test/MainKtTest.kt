9ab8e8b89d133631e121c94aa33eb8fbc88eb08b
Leandro Zuza <lendro.zuza@connectbr.dev>
2025-06-10 00:13:34 -0300

    [FEAT]: MUMBLING

---
+++ mumbling/test/MainKtTest.kt
@@ -0,0 +1,18 @@
+import org.testng.Assert.assertEquals
+import org.testng.annotations.Test
+
+class MainKtTest {
+    @Test
+    fun sampleTests() {
+        testing("ZpglnRxqenU", "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+        testing("NyffsGeyylB", "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+        testing("MjtkuBovqrU", "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+        testing("EvidjUnokmM", "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+        testing("HbideVbxncC", "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
+    }
+
+    private fun testing(input: String, expected:String) {
+        val actual_ = accum(input)
+         assertEquals(expected, actual_, "for string : \"" + input + "\"\n")
+    }
+}
